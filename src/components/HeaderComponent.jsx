33bf738d09396c9b9f1ba0d710b3720b96b33b97
guilhermemedeirosmergulhao <gui.mergulhao55@outlook.com>
2023-07-20 16:35:45 -0300

    Adicionando um header

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,22 @@
+import './HeaderStyle.css'
+import { Link } from 'react-router-dom'
+
+export default function HeaderComponent(){
+    return(
+        <header>
+            <div className='container'>
+                <div className='row'>
+                    <nav className="navbar navbar-expand-lg">
+                        <label className="navbar-brand text-info ms-2 fs-2 fw-bold">For the Night</label>
+                        <div className="collapse navbar-collapse"/>
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5">
+                                <Link className="nav-link text-info" to="/">+ Games</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
--- src/components/WingsJourneyApp.jsx
+++ src/components/WingsJourneyApp.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import GamesListComponent from './GamesListComponent'
+import HeaderComponent from './HeaderComponent'
 
 export default function WingsJourneyApp(){
     return (
         <div className="WingsJourneyApp">
             <BrowserRouter>
+                <HeaderComponent/>
                 <Routes>
-                <Route path='/' element={<GamesListComponent/>}/>
+                    <Route path='/' element={<GamesListComponent/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
